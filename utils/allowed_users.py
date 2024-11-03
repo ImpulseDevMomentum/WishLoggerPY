e43af6884ec0a2ef4381a1ff67bbed7d34cb25f6
lis0ownik <97831192+lis0ownik@users.noreply.github.com>
2024-11-03 20:54:42 +0100

    added information about allowed_users.py

--- utils/allowed_users.py
+++ utils/allowed_users.py
@@ -1,1 +1,6 @@
+# Past your user id here
+# EXAMPLE: allowed_user_ids = ["1234567890"]
+# add more users after comma
+# EXAMPLE: allowed_user_ids = ["1234567890", "0987654321"]
+
 allowed_user_ids = []
